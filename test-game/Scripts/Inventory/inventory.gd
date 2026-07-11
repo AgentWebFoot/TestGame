c21e7bcf683ea44073756a3581c8902e255fd00c
StormCloak34 <232502083+StormCloak34@users.noreply.github.com>
2026-07-11 01:39:07 -0400

    Finished weight_label and keybinds
    Finished WeightLabel the current font is set to 32px, the data is appended to the label.
    
    Added keybinds for dropping items and splitting stacks, but they do not have code to work yet.

--- test-game/Scripts/Inventory/inventory.gd
+++ test-game/Scripts/Inventory/inventory.gd
@@ -3,13 +3,26 @@ extends PanelContainer
 @export var items: Array[ItemData] = []
 @export var inventory_item_scene: PackedScene
 @onready var item_grid: GridContainer = %InventoryGrid
+@onready var weight_label: Label = %WeightLabel
+
+var current_weight: float
 
 func _ready() -> void:
 	for i in items:
 		add_item(i)
+		determine_weight(i)
+	weight_label_append()
 
 func add_item(item_data: ItemData) -> void:
 	var inventory_item = inventory_item_scene.instantiate()
 	inventory_item.data = item_data
 	add_child(inventory_item)
 	var success = item_grid.attempt_to_add_item_data(inventory_item)
+
+func determine_weight(item_data: ItemData) -> void:
+	var inventory_item = inventory_item_scene.instantiate()
+	inventory_item.data = item_data
+	current_weight += item_data.weight
+
+func weight_label_append() -> void:
+	weight_label.text += str(current_weight) + "/100.0"
